37eecfeefdb881e57c12c538f81ba3715d15dbba
rcastril <rcastril@br.ibm.com>
2016-04-08 18:29:13 -0300

    Cosmetic

--- modules/log/syslog.py
+++ modules/log/syslog.py
@@ -1,30 +1,31 @@
-'''
+"""
 Created on Mar 31, 2016
 
 @author: jpmenega
-'''
+"""
 import logging
 
+
 def getLogger(module):
-    #logging.basicConfig(filename='log/colector.log', level=logging.DEBUG, format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
-    #logger = logging.getLogger(module)
+    # logging.basicConfig(filename='log/colector.log', level=logging.DEBUG, format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+    # logger = logging.getLogger(module)
 
     logger = logging.getLogger(module)
     logger.setLevel(logging.DEBUG)
 
     formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
 
-    #File Handler
+    # File Handler
     fh = logging.FileHandler('log/colector.log')
     fh.setLevel(logging.INFO)
     fh.setFormatter(formatter)
     logger.addHandler(fh)
 
-    #Console Handler
-    #ch = logging.StreamHandler()
-    #ch.setLevel(logging.DEBUG)
-    #ch.setFormatter(formatter)
-    #logger.addHandler(ch)
+    # Console Handler
+    # ch = logging.StreamHandler()
+    # ch.setLevel(logging.DEBUG)
+    # ch.setFormatter(formatter)
+    # logger.addHandler(ch)
 
     return logger
 

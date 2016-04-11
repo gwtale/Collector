c1e4946e9eaa9145bf642a53d053b231e88dc124
rcastril <rcastril@br.ibm.com>
2016-04-11 17:50:17 -0300

    scp working

--- modules/tools/scpWrapper.py
+++ modules/tools/scpWrapper.py
@@ -23,22 +23,21 @@ def send(fileToSend, remotePath=None, remoteFileName=None):
     if remotePath is None:
         remotePath = base['sherlockCachePath']
     if remoteFileName is None:
-        remoteFileName = ""
+        remoteFileName = str(fileToSend)
 
     # Vars
-    ssh = paramiko.SSHClient()
     localFilePath = base['cache_path'] + str(fileToSend)
     remoteFilePath = remotePath + remoteFileName
     host = base['sherlockIp']
+    paramiko.util.log_to_file('log/sftp.paramiko.log')
 
     try:
-        ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-        ssh.connect(host, username=base['scp_ssh_id'], password=base['scp_ssh_pwd'])
+        t = paramiko.Transport((host, 22))
+        t.connect(username=base['scp_ssh_id'], password=base['scp_ssh_pwd'])
         # Copy remote file to server
-        sftp = ssh.open_sftp()
-        sftp.put(localFilePath, remoteFilePath)
+        sftp = paramiko.SFTPClient.from_transport(t)
+        sftp.put(str(localFilePath), str(remoteFilePath))
         sftp.close()
-        ssh.close()
 
         return True
     except Exception as detail:

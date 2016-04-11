6dfcd87eef3241e337f6d52aa3e552f6086ec920
rcastril <rcastril@br.ibm.com>
2016-04-11 13:59:52 -0300

    removed collector.log

--- cron_uploadCache.py
+++ cron_uploadCache.py
@@ -1,4 +1,6 @@
-import pymysql, json, os, sys
+import pymysql, sys
+from os import path, listdir, remove
+from json import load
 import modules.log.syslog
 import modules.tools.scp as scp
 
@@ -12,27 +14,27 @@ logger = modules.log.syslog.getLogger("cron_uploadCache")
 logger.info('cron_uploadCache.py started.')
 
 # Load base configuration
-if os.path.exists(baseConfig):
+if path.exists(baseConfig):
     with open(baseConfig) as infile:
-        base = json.load(infile)
+        base = load(infile)
 else:
     logger.error('Not able to retrieve config/base.json')
     sys.exit()
 
 # Check whether there is job to do
-cachedCount = len([name for name in os.listdir(base['cache_path'])])
+cachedCount = len([name for name in listdir(base['cache_path'])])
 if cachedCount == 0:
     logger.info('No job to do. Exiting...')
     sys.exit()
 
 # There is work
 # Loop through EACH cached file
-for cache_file in os.listdir(base['cache_path']):
+for cache_file in listdir(base['cache_path']):
     try:
         # scp to Sherlock
         scp.send(cache_file)
         # Delete uploaded file
-        os.remove(base['cache_path'] + cache_file)
+        remove(base['cache_path'] + cache_file)
         logger.info('Cache file uploaded and deleted from local cache: {0}'.format(str(cache_file)))
     except Exception as detail:
         logger.error('Not able to upload cache file {0}. Error message: {1}'.format(str(cache_file), str(detail)))

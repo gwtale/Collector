1f687a2d4a348e412a4eef2244adfdbd0d215f2c
root <rcastril@br.ibm.com>
2018-02-15 02:04:35 -0200

    Deleting zbxScripts/cron_installZbxAgent.sh

--- zbxScripts/cron_installZbxAgent.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-IP=$1
-ID=$2
-PW=$3
-
-rm ~/.ssh/known_hosts
-cd /var/Collector/zbxScripts
-if netstat -l|grep 8000 > /dev/null; then echo 0 > /dev/null; else echo "waiting for simpleHTTPserver" && exit; fi
-if curl -s http://$IP:10050 > /dev/null; then echo "agent is listening"; else ./installZbxAgent.exp $IP $ID "$PW" && echo "installZbxAgent.exp executed"; fi
